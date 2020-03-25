4bc700ba3a1fe818bdf9ba096936902a52b600f1
pbedrosian <peter_bedrosian@Yahoo.com>
2020-03-25 15:14:32 -0400

    This is my third commit

--- lib/pokemon_cli/api.rb
+++ lib/pokemon_cli/api.rb
@@ -4,22 +4,15 @@ class API
   def self.info(index)
     url = "https://pokeapi.co/api/v2/pokemon/#{index}/"
     info = HTTParty.get(url)
-
     name = info["species"].fetch("name").capitalize # -- this gets the pokemon name
-
-
     full_stats = info["stats"]
     stats = full_stats.collect {|n| n.fetch("base_stat")} # -- this gets the stats
-
     index = info["id"] # -- this gets the pokemon index
-
     find_type = info["types"]
     type = find_type.collect {|n| n["type"].fetch("name")}
 
     @@all_stats += [name, stats, index, type]
-
     index = Pokemon.new
-
     self.all.clear
   end
 
